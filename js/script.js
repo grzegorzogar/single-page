1d964d01e5169c11cb7f5d8000fc29b509b4bb0e
Mateusz Ogar <mateusz.ogar@azimo.com>
2018-10-28 19:14:11 +0100

    script fixed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-let arrow = document.getElementsByClassName('arrow'); // czysty js
-
-
 let $arrowjQuery = $(".arrow");  // pobieranie klasy arrow przez jQuery
 
 let off = $("#container").offset().top;
 
-$arrowjQuery.on("click", function () {
-    $("body").animate({
-        scrollTop: $("#container").offset().top
-    }, 1000)
+$arrowjQuery.click(function () {
+    $("html,body").animate({
+            scrollTop: off
+        },
+        1500);
 });
+
+
+
